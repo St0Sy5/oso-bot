53447eea2e226c03099f9192f993387951284429
St0Sy5 <drumm3r09@gmail.com>
2018-02-22 23:24:40 -0600

    Resolved incompatible python issues v2

--- bot.py
+++ bot.py
@@ -4,46 +4,35 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='$$', description='A bot that greets the user back.')
 
 @bot.event
-#async def
-@asyncio.coroutine on_ready():
+async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
 @bot.command()
-#async def
-@asyncio.coroutine add(ctx, a: int, b: int):
-    #await
-    yield from ctx.send(a+b)
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
 
 @bot.command()
-#async def
-@asyncio.coroutine multiply(ctx, a: int, b: int):
-    #await
-    yield from ctx.send(a*b)
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
 
 @bot.command()
-#async def
-@asyncio.coroutine greet(ctx):
-    #await
-    yield from ctx.send(":smiley: :wave: Hello, there!")
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
 @bot.command()
-#async def
-@asyncio.coroutine cat(ctx):
-    #await
-    yield from ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
 @bot.command()
-#async def
-@asyncio.coroutine info(ctx):
+async def info(ctx):
     embed = discord.Embed(title="OSO-bot", description='A bear-y good bot!')
     embed.add_field(name="Author", value="PythonSlut#0477")
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
     embed.add_field(name="Invite", value="https://discordapp.com/api/oauth2/authorize?client_id=416349280597573632&permissions=2048&scope=bot")
-    #await
-    yield from ctx.send(embed=embed)
+    await ctx.send(embed=embed)
 
 #bot.remove_command('help')
 
