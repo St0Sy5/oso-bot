e06d048d00485d304952776336631022e8308e3e
St0Sy5 <drumm3r09@gmail.com>
2018-02-22 23:19:00 -0600

    Resolved incompatible python issues

--- bot.py
+++ bot.py
@@ -4,47 +4,58 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='$$', description='A bot that greets the user back.')
 
 @bot.event
-async def on_ready():
+#async def
+@asyncio.coroutine on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
 @bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
+#async def
+@asyncio.coroutine add(ctx, a: int, b: int):
+    #await
+    yield from ctx.send(a+b)
 
 @bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
+#async def
+@asyncio.coroutine multiply(ctx, a: int, b: int):
+    #await
+    yield from ctx.send(a*b)
 
 @bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
+#async def
+@asyncio.coroutine greet(ctx):
+    #await
+    yield from ctx.send(":smiley: :wave: Hello, there!")
 
 @bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+#async def
+@asyncio.coroutine cat(ctx):
+    #await
+    yield from ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
 @bot.command()
-async def info(ctx):
+#async def
+@asyncio.coroutine info(ctx):
     embed = discord.Embed(title="OSO-bot", description='A bear-y good bot!')
     embed.add_field(name="Author", value="PythonSlut#0477")
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
     embed.add_field(name="Invite", value="https://discordapp.com/api/oauth2/authorize?client_id=416349280597573632&permissions=2048&scope=bot")
-    await ctx.send(embed=embed)
+    #await
+    yield from ctx.send(embed=embed)
 
-bot.remove_command('help')
+#bot.remove_command('help')
 
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="OSO-bot", description="A bear-y good bot!")
-    embed.add_field(name="$$add X Y", value="Adds **X** and **Y** together", inline=False)
-    embed.add_field(name="$$multiply X Y", value="Multiplies **X** and **Y** together", inline=False)
-    embed.add_field(name="$$greet", value="Sends a nice message of greeting", inline=False)
-    embed.add_field(name="$$cat", value="Provides a cat gif for eyebleach", inline=False)
-    embed.add_field(name="$$info", value="Prints some info about oso-bot", inline=False)
-    embed.add_field(name="$$help", value="Prints this message", inline=False)
-    await ctx.send(embed=embed)
+#@bot.command()
+#async def help(ctx):
+#    embed = discord.Embed(title="OSO-bot", description="A bear-y good bot!")
+#    embed.add_field(name="$$add X Y", value="Adds **X** and **Y** together", inline=False)
+#    embed.add_field(name="$$multiply X Y", value="Multiplies **X** and **Y** together", inline=False)
+#    embed.add_field(name="$$greet", value="Sends a nice message of greeting", inline=False)
+#    embed.add_field(name="$$cat", value="Provides a cat gif for eyebleach", inline=False)
+#    embed.add_field(name="$$info", value="Prints some info about oso-bot", inline=False)
+#    embed.add_field(name="$$help", value="Prints this message", inline=False)
+#    await ctx.send(embed=embed)
 
 bot.run('NDE2MzQ5MjgwNTk3NTczNjMy.DXDTZA.d66DpwRthcczhn0yM1epdrH4UsM')
